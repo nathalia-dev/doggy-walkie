8dcb663ea1fe161e7fa58eeb114afb5f4ea36644
Nathalia <62966004+nathalia-dev@users.noreply.github.com>
2022-03-18 09:49:53 -0400

    changing database_url

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgres:///doggy_walkie'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
