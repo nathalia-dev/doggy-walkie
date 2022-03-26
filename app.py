e0766a3c0d92af23e4c30a34f74cf6f18a448bb3
Nathalia <62966004+nathalia-dev@users.noreply.github.com>
2022-03-26 08:38:06 -0400

    adjusting app.config for heroku

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgres:///doggy_walkie'))
+#to local use: app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgres:///doggy_walkie'))
 
-# to heroku: os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+# to heroku: app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL').replace("://", "ql://", 1))
 
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL').replace("://", "ql://", 1))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
